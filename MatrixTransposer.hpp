b8b5c11a5989e571b113255c3dc0d931e80b0def
brunomaga <brunomaga@gmail.com>
2018-11-12 15:46:14 +0100

    code clean-up

--- MatrixTransposer.hpp
+++ MatrixTransposer.hpp
@@ -9,16 +9,16 @@ class MatrixTransposer {
  private:
   struct Metadata {
     void* cells;
-    int cpu;
+    int rank;
     unsigned int row;
     unsigned int col;
     unsigned int cell_count;
   };
 
-  static int QSortCompare(const void* _a, const void* _b) {
-    Metadata* a = static_cast<Metadata*>((void*)_a);
-    Metadata* b = static_cast<Metadata*>((void*)_b);
-    if (a->cpu != b->cpu) return (a->cpu - b->cpu);
+  static int QSortCompare(const void* md_a, const void* md_b) {
+    Metadata* a = static_cast<Metadata*>((void*)md_a);
+    Metadata* b = static_cast<Metadata*>((void*)md_b);
+    if (a->rank != b->rank) return (a->rank - b->rank);
     if (a->row != b->row) return (a->row - b->row);
     assert(a->col != b->col);
     return (a->col - b->col);
@@ -29,9 +29,9 @@ class MatrixTransposer {
                                       Metadata* metadatas, T*& cells) {
     qsort(metadatas, col_count, sizeof(Metadata), QSortCompare);
 
-    // now that the qsort ir ordered correctly, we will shuffle the elements
-    // of cells to follow the same order (by looking at the pointers of the
-    // structure data)
+    /* now that the qsort is ordered correctly, we will shuffle the elements
+       of cells to follow the same order (by looking at the pointers of the
+       structure data) */
     unsigned long long cell_id = 0;
     T* cells_temp = new T[cell_count];
     for (unsigned int c = 0; c < col_count; c++) {
@@ -74,7 +74,6 @@ class MatrixTransposer {
                         unsigned int*& cell_counts, MPI_Comm comm) {
     // MPI variables
     int mpi_size = -1, mpi_rank = -1;
-    ;
     MPI_Comm_rank(comm, &mpi_rank);
     MPI_Comm_size(comm, &mpi_size);
 
@@ -88,32 +87,33 @@ class MatrixTransposer {
     unsigned int my_col_count = 0;
     for (unsigned int r = 0; r < my_row_count; r++) my_col_count += counts[r];
 
-    // zero step: replace a matrix into 'mpiSize' sub-matrices transposed
+    // local transpose
     Metadata* metadatas = new Metadata[my_col_count];
 
-    // we will divide this L lines in 'mpiSize' matrices, and transpose each
-    // one of them. For that we perform a sorting based on destinationCPU, then
-    // column Id (since its the row for transposed matrix), and then on row
-    //(transposed matrix column)
+    /* we divide this L lines in 'mpi_size' matrices, and transpose each
+       them: we perform a sorting based on target rank, then col id (since its
+       the row for transposed matrix), and then on row (transposed column)*/
 
-    // keeps the map of which cpu takes each neuron
-    unsigned int total_row_count = 0;
-    MPI_Allreduce(&my_row_count, &total_row_count, 1, MPI_UNSIGNED, MPI_SUM,
+    unsigned int total_row_count = rank_end_offsets[mpi_size - 1];
+#ifndef NDEBUG  // TODO delete (debug step only)
+    unsigned int total_row_count_1 = 0;
+    MPI_Allreduce(&my_row_count, &total_row_count_1, 1, MPI_UNSIGNED, MPI_SUM,
                   MPI_COMM_WORLD);
+    assert(total_row_count == total_row_count_1);
+#endif
 
-    unsigned int* cpu_per_row_id = new unsigned int[total_row_count];
-    unsigned int cpu_id = 0;
-    for (unsigned int n = 0; n < (unsigned int)total_row_count; n++) {
-      assert(cpu_id < (unsigned int)mpi_size);
-
-      cpu_per_row_id[n] = cpu_id;
-      while (cpu_id < static_cast<unsigned int>(mpi_size) &&
-             rank_end_offsets[cpu_id] <= n) {
-        cpu_id++;
-      }
+    // builds a map of which rank is assinged to each row
+    unsigned int* rank_per_row_id = new unsigned int[total_row_count];
+    unsigned int rank_id = 0;
+    for (unsigned int n = 0; n < total_row_count; n++) {
+      assert(rank_id < (unsigned int)mpi_size);
+      rank_per_row_id[n] = rank_id;
+      while (rank_id < static_cast<unsigned int>(mpi_size) &&
+             rank_end_offsets[rank_id] <= n)
+        rank_id++;
     }
 
-    // converting the Sparse matrix arrays into a qsort wrapper structure
+    // create metadata structures
     unsigned int cell_id = 0, col_id = 0, total_cell_count = 0;
     for (unsigned int rowId = 0; rowId < my_row_count; rowId++) {
       for (unsigned int c = 0; c < counts[rowId]; c++) {
@@ -122,21 +122,18 @@ class MatrixTransposer {
 
         total_cell_count += cell_counts[col_id];
 
-        // we intentionally swapped row and column, to force
-        // the qsort to sort them like this (therefor transposing it)
+        // we swap row and column, to force qsort to transpose when sorting
         unsigned int& columnId = displs[col_id];
         metadatas[col_id].row = columnId;
         metadatas[col_id].col = my_first_row_id + rowId;
-        metadatas[col_id].cpu = cpu_per_row_id[columnId];
-
-        // increment cell and column Id
+        metadatas[col_id].rank = rank_per_row_id[columnId];
         cell_id += cell_counts[col_id];
         col_id++;
       }
     }
 
-    delete[] cpu_per_row_id;
-    cpu_per_row_id = NULL;
+    delete[] rank_per_row_id;
+    rank_per_row_id = NULL;
     delete[] displs;
     displs = NULL;
     delete[] cell_counts;
@@ -146,31 +143,28 @@ class MatrixTransposer {
 
     SortCellsByCpuRowColumn(my_col_count, total_cell_count, metadatas, cells);
 
-    // 2nd step: now we will send the respective table info to each CPU
-    // (qsortStruct)
-    int* sendcounts = new int[mpi_size];
-    int* recvcounts = new int[mpi_size];
-    int* sentdispls = new int[mpi_size];
-    int* recvdispls = new int[mpi_size];
+    // View Swap step 1/2: metadata matrix transose
+    int* sendcounts = new int[mpi_size]();
+    int* recvcounts = new int[mpi_size]();
+    int* sentdispls = new int[mpi_size]();
+    int* recvdispls = new int[mpi_size]();
 
-    for (int cpu = 0; cpu < mpi_size; cpu++) sendcounts[cpu] = 0;
+#ifndef NDEBUG  // TODO delete
+    for (int r = 0; r < mpi_size; r++) assert(sendcounts[r]) == 0;
+#endif
 
-    for (unsigned int c = 0; c < my_col_count; c++) {
-      int& cpu = metadatas[c].cpu;
-      sendcounts[cpu] += sizeof(Metadata);
-    }
+    for (unsigned int c = 0; c < my_col_count; c++)
+      sendcounts[metadatas[c].rank] += sizeof(Metadata);
 
-    // share the amount of data to be received by each other cpu
+    // exchange metadata sizes to be received by each rank
     MPI_Alltoall(sendcounts, 1, MPI_INT, recvcounts, 1, MPI_INT, comm);
 
     // calculate offset for the data sent/received
-    for (int cpu = 0; cpu < mpi_size; cpu++)
-      sentdispls[cpu] =
-          cpu == 0 ? 0 : sentdispls[cpu - 1] + sendcounts[cpu - 1];
+    for (int r = 0; r < mpi_size; r++)
+      sentdispls[r] = r == 0 ? 0 : sentdispls[r - 1] + sendcounts[r - 1];
 
-    for (int cpu = 0; cpu < mpi_size; cpu++)
-      recvdispls[cpu] =
-          cpu == 0 ? 0 : recvdispls[cpu - 1] + recvcounts[cpu - 1];
+    for (int r = 0; r < mpi_size; r++)
+      recvdispls[r] = r == 0 ? 0 : recvdispls[r - 1] + recvcounts[r - 1];
 
     // calculate total data size to be received
     unsigned int my_col_count_T =
@@ -178,34 +172,30 @@ class MatrixTransposer {
         sizeof(Metadata);
     Metadata* metadatas_T = new Metadata[my_col_count_T];
 
-    // send around the table structure of the elements to be received
+    // exchange metadata structures
     MPI_Alltoallv(metadatas, sendcounts, sentdispls, MPI_BYTE, metadatas_T,
                   recvcounts, recvdispls, MPI_BYTE, comm);
 
-    // 3rd step: now we will send the respective cells to each CPU
-    for (int cpu = 0; cpu < mpi_size; cpu++) sendcounts[cpu] = 0;
+    // View Swap step 2/2: elements exchange
+    for (int r = 0; r < mpi_size; r++) sendcounts[r] = 0;
 
-    for (unsigned int c = 0; c < my_col_count; c++) {
-      int& cpu = metadatas[c].cpu;
-      sendcounts[cpu] += metadatas[c].cell_count * sizeof(T);
-    }
+    for (unsigned int c = 0; c < my_col_count; c++)
+      sendcounts[metadatas[c].rank] += metadatas[c].cell_count * sizeof(T);
 
     delete[] metadatas;
     metadatas = NULL;
 
-    // share the amount of data to be received by each other cpu
+    // exchange element sizes to be received by each rank
     MPI_Alltoall(sendcounts, 1, MPI_INT, recvcounts, 1, MPI_INT, comm);
 
     // calculate offset for the data sent/received
-    for (int cpu = 0; cpu < mpi_size; cpu++)
-      sentdispls[cpu] =
-          cpu == 0 ? 0 : sentdispls[cpu - 1] + sendcounts[cpu - 1];
+    for (int r = 0; r < mpi_size; r++)
+      sentdispls[r] = r == 0 ? 0 : sentdispls[r - 1] + sendcounts[r - 1];
 
-    for (int cpu = 0; cpu < mpi_size; cpu++)
-      recvdispls[cpu] =
-          cpu == 0 ? 0 : recvdispls[cpu - 1] + recvcounts[cpu - 1];
+    for (int r = 0; r < mpi_size; r++)
+      recvdispls[r] = r == 0 ? 0 : recvdispls[r - 1] + recvcounts[r - 1];
 
-    // receives data
+    // exchange elements
     unsigned int total_cell_count_T =
         (recvdispls[mpi_size - 1] + recvcounts[mpi_size - 1]) / sizeof(T);
     T* cells_T = new T[total_cell_count_T];
@@ -236,15 +226,14 @@ class MatrixTransposer {
       metadatas[c].cells = &(cells[cell_id]);
       cell_id += metadatas[c].cell_count;
 
-      // make sure that all columns received were meant for this cpu
-      assert(metadatas[c].cpu == mpi_rank);
+      // make sure that all columns received were meant for this rank
+      assert(metadatas[c].rank == mpi_rank);
     }
 
-    // we sort by row, not by cpu (therefore converting into a single sparse
-    // matrix)
+    // we sort by row, not by rank, therefore converting into single matrix
     SortCellsByCpuRowColumn(my_col_count, total_cell_count, metadatas, cells);
 
-    // set final data structures: convert qsort wrapper to sparse matrix arrays
+    // final data structures: use metadata to retrieve sparse matrix arrays
     displs = new unsigned int[my_col_count];
     counts = new unsigned int[my_row_count];
     cell_counts = new unsigned int[my_col_count];
--- main.cpp
+++ main.cpp
@@ -1,134 +1,91 @@
 #include "MatrixTransposer.hpp"
 
+#include <time.h>
 #include <cassert>
 #include <cstdio>
 #include <cstdlib>
 
-#define OUTPUT_DEBUG_INFO
-
+/**
+ * @brief GenerateRandomNumber returns a random value in interval [min,max)
+ * @param min minimum random value
+ * @param max maximum random value
+ * @return
+ */
 int GenerateRandomNumber(int min, int max) {
   return rand() % (max - min) + min;
 }
 
+/**
+ * @brief main Generates a random sparse matrix and transposes it
+ * @return
+ */
 int main(int argv, char** argc) {
-  MPI_Init(&argv, &argc);
-
-  // MPI variables
+  // MPI and random seed init
   int mpi_size = -1, mpi_rank = -1;
+  MPI_Init(&argv, &argc);
   MPI_Comm_rank(MPI_COMM_WORLD, &mpi_rank);
   MPI_Comm_size(MPI_COMM_WORLD, &mpi_size);
-
-  // TEST CASE 2
   srand(time(NULL) / (mpi_rank + 1));
 
-  unsigned int row_count = 0;
-  unsigned int* rank_end_offsets;
-  unsigned int* counts;
-  unsigned int* displs;
-  unsigned int* cell_counts;
-  int* cells;
+  unsigned int row_count = GenerateRandomNumber(3, 4);
 
-  unsigned long long col_id = 0, cell_id = 0;
-  (void)col_id;
-  (void)cell_id;  // clear Unused Var warning;
-
-  rank_end_offsets = new unsigned int[mpi_size];
-
-  // row_count = GenerateRandomNumber (10000, 100000); //1M and 10M;
-  row_count = GenerateRandomNumber(3, 4);  // 1M and 10M;
-
-  // rank_end_offsets
+  // rank end offsets and first row id of this rank
+  unsigned int total_row_count = 0;
   unsigned int* rank_row_counts = new unsigned int[mpi_size];
   MPI_Allgather(&row_count, 1, MPI_UNSIGNED, rank_row_counts, 1, MPI_UNSIGNED,
                 MPI_COMM_WORLD);
-  for (int r = 0; r < mpi_size; r++)
-    rank_end_offsets[r] = r == 0 ? rank_row_counts[r] - 1
-                                 : rank_row_counts[r] + rank_end_offsets[r - 1];
-  unsigned int my_first_row_id =
-      mpi_rank == 0 ? 0 : rank_end_offsets[mpi_rank - 1] + 1;
-
-  unsigned int total_row_count = 0;
   MPI_Allreduce(&row_count, &total_row_count, 1, MPI_UNSIGNED, MPI_SUM,
                 MPI_COMM_WORLD);
 
-  // colsPerRow
-  counts = new unsigned int[row_count];
+  unsigned int* rank_end_offsets = new unsigned int[mpi_size];
+  for (int r = 0; r < mpi_size; r++)
+    rank_end_offsets[r] = r == 0 ? rank_row_counts[r] - 1
+                                 : rank_row_counts[r] + rank_end_offsets[r - 1];
+  unsigned int first_row_id =
+      mpi_rank == 0 ? 0 : rank_end_offsets[mpi_rank - 1] + 1;
 
-  long long my_col_count = 0, total_column_count = 0;
-  ;
+  unsigned int* counts = new unsigned int[row_count];
+
+  long long col_count = 0, total_column_count = 0;
   for (unsigned int i = 0; i < row_count; i++) {
-    // colsPerRow[i] = generateRandomNumber (100, 1000);
+    // counts[i] = GenerateRandomNumber (1, row_count);
     counts[i] = 2;
-    my_col_count += counts[i];
+    col_count += counts[i];
   }
 
-  long long my_cell_count = 0, total_cell_count = 0;
-
-  displs = new unsigned int[my_col_count];
-  cell_counts = new unsigned int[my_col_count];
-  col_id = 0;
+  unsigned int* displs = new unsigned int[col_count];
+  unsigned int* cell_counts = new unsigned int[col_count];
+  long long col_id = 0, cell_count = 0, total_cell_count = 0;
   for (unsigned int i = 0; i < row_count; i++) {
     for (unsigned int j = 0; j < counts[i]; j++) {
       displs[col_id] = total_row_count * j / counts[i];
       assert(displs[col_id] < total_row_count);
       cell_counts[col_id] = 1;
-      // cellsPerCol[colId]= generateRandomNumber(2,20);
-      my_cell_count += cell_counts[col_id];
+      // cell_count[col_id]= GenerateRandomNumber(2,20);
+      cell_count += cell_counts[col_id];
       col_id++;
     }
   }
 
-  MPI_Allreduce(&my_cell_count, &total_cell_count, 1, MPI_LONG_LONG, MPI_SUM,
+  MPI_Allreduce(&cell_count, &total_cell_count, 1, MPI_LONG_LONG, MPI_SUM,
                 MPI_COMM_WORLD);
-  MPI_Allreduce(&my_col_count, &total_column_count, 1, MPI_LONG_LONG, MPI_SUM,
+  MPI_Allreduce(&col_count, &total_column_count, 1, MPI_LONG_LONG, MPI_SUM,
                 MPI_COMM_WORLD);
-  if (mpi_rank == 0) printf("TOTAL COLUMNS COUNT %lld\n", total_column_count);
-  if (mpi_rank == 0) printf("TOTAL CELLS COUNT %lld\n", total_cell_count);
+  if (mpi_rank == 0) printf("total column count: %lld\n", total_column_count);
+  if (mpi_rank == 0) printf("total cell count: %lld\n", total_cell_count);
 
-  cells = new int[my_cell_count];
-  for (unsigned int i = 0; i < my_cell_count; i++)
+  int* cells = new int[cell_count];
+  for (unsigned int i = 0; i < cell_count; i++)
     cells[i] = GenerateRandomNumber(0, 8);
 
-/*    //TEST CASE 1
-
-        unsigned int _endRowCpus[4] = {3,8,11,16};
-        endRowCpus = _endRowCpus;
-
-      if (mpiRank % 2 == 0)
-      {
-        numberOfRows=3;
-        unsigned int _colsPerRow[3] = {4,4,0};
-        unsigned int _colsIndex[8] =  {0, 3, 9, 13, 3, 8, 12, 15};
-        unsigned int _cellsPerCol[8] = {1,4,1,1,3,1,4,1};
-        int _cells[16] = {0,1,2,3,4,2,3,4,5,6,5,6,7,8,9,7};
-
-        colsPerRow = _colsPerRow;
-        colsIndex = _colsIndex;
-        cellsPerCol = _cellsPerCol;
-        cells = _cells;
-      }
-      else
-      {
-        numberOfRows=5;
-        unsigned int _colsPerRow[5] = {4,3,3,0,1};
-        unsigned int _colsIndex[11] =  {1, 4, 10, 13, 1, 5, 9, 3, 8, 12, 15};
-        unsigned int _cellsPerCol[11] = {1,4,1,1,3,1,4,1,1,2,3};
-        int _cells[22] = {0,1,2,3,4,2,3,4,5,6,5,6,7,8,9,7,8,9,0,0,1,2};
-
-        colsPerRow = _colsPerRow;
-        colsIndex = _colsIndex;
-        cellsPerCol = _cellsPerCol;
-        cells = _cells;
-     }
-     */
-
-#ifdef OUTPUT_DEBUG_INFO
-  col_id = 0, cell_id = 0;
+#ifndef NDEBUG
+  col_id = 0;
+  unsigned long long cell_id = 0;
   for (int i = 0; i < mpi_size; i++) {
     if (mpi_rank == i) {
-      fprintf(stderr, "RANK %d\n", mpi_rank);
+      fprintf(stderr, "mpi rank: %d\n", mpi_rank);
       for (unsigned int r = 0; r < row_count; r++) {
-        fprintf(stderr, "ROW %d (%d cols) ::", r + my_first_row_id, counts[r]);
+        fprintf(stderr, "row: %d (%d cols) ::", r + first_row_id, counts[r]);
         for (unsigned int c = 0; c < counts[r]; c++) {
           fprintf(stderr, "[id %d, %d cell] ", displs[col_id],
                   cell_counts[col_id]);
@@ -140,22 +97,26 @@ int main(int argv, char** argc) {
         fprintf(stderr, "\n");
       }
     }
-    MPI_Barrier(MPI_COMM_WORLD);
     fprintf(stderr, "\n");
   }
-  if (mpi_rank == 0) fprintf(stderr, "Start\n");
 #endif
 
+  MPI_Barrier(MPI_COMM_WORLD);
+  if (mpi_rank == 0) fprintf(stderr, "Started.\n");
+  clock_t t_start = clock();
   MatrixTransposer<int>::Transpose(rank_end_offsets, cells, counts, displs,
                                    cell_counts, MPI_COMM_WORLD);
+  if (mpi_rank == 0)
+    fprintf(stderr, "Finished. Time taken: %.2fs\n",
+            (double)(clock() - t_start) / CLOCKS_PER_SEC);
 
-#ifdef OUTPUT_DEBUG_INFO
+#ifndef NDEBUG
   col_id = 0, cell_id = 0;
   for (int i = 0; i < mpi_size; i++) {
     if (mpi_rank == i) {
       fprintf(stderr, "RANK %d\n", mpi_rank);
       for (unsigned int r = 0; r < row_count; r++) {
-        fprintf(stderr, "ROW %d (%d cols) ::", r + my_first_row_id, counts[r]);
+        fprintf(stderr, "ROW %d (%d cols) ::", r + first_row_id, counts[r]);
         for (unsigned int c = 0; c < counts[r]; c++) {
           fprintf(stderr, "[id %d, %d cell] ", displs[col_id],
                   cell_counts[col_id]);
@@ -170,7 +131,6 @@ int main(int argv, char** argc) {
     MPI_Barrier(MPI_COMM_WORLD);
     fprintf(stderr, "\n");
   }
-  if (mpi_rank == 0) fprintf(stderr, "Finish\n");
 #endif
 
   return 0;
